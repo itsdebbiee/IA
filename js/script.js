dba5fa01a0284eee829f0ab57b9e60c2316633cc
Debbie Pan <xdebbiee@gmail.com>
2014-02-23 19:33:56 -0800

    minor changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $(".headlines").click(function(e) {
     e.preventDefault();
+
+    $(".headlines").fadeOut('fast');
     $(".headline-content").fadeIn('fast');
 
   });
